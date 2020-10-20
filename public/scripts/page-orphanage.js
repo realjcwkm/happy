fe8d0a21a84495f551031079f6c72f4193f0f745
realjcwkm <realjcwkm@gmail.com>
2020-10-20 14:43:13 -0400

    Adicionando mapa fixo no orfanato

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,35 @@
+const options = {
+  dragging: false,
+  touchZone: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
+}
+
+// setView([lat, long], zoom)
+const map = L.map( 'mapid', options ).setView( [-27.2200772, -49.6482579], 15 );
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo( map );
+
+const icon = L.icon( {
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+} );
+
+const popup = L.popup( {
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240,
+} ).setContent( 'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>' );
+
+L.marker( [-27.2200772, -49.6482579], { icon: icon } )
+  .addTo( map );
+
+function selectImage( event ) {
+
+}
