0399411cef2d2ca431e9f2de6425d4e694e74d35
realjcwkm <realjcwkm@gmail.com>
2020-10-19 22:52:29 -0400

    Adicionando mapa

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,24 @@
+// setView([lat, long], zoom)
+const map = L.map( 'mapid' ).setView( [-27.2200772, -49.6482579], 15 );
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo( map );
+
+const icon = L.icon( {
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+} );
+
+const popup = L.popup( {
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240,
+} ).setContent( 'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>' );
+
+L.marker( [-27.2200772, -49.6482579], { icon: icon } )
+  .addTo( map )
+  .bindPopup( popup );
