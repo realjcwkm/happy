acae0e0044639ee0f7cc2271fd8750cb0f043a8a
realjcwkm <realjcwkm@gmail.com>
2020-10-21 15:25:19 -0400

    Usando o handlebars

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const pages = require("./pages.js");
+const server = express();
+
+server.use(express.static("public"))
+  .set("views", path.join(__dirname, "views"))    //configura o template engine
+  .set("view engine", "hbs")                      //configura a engine
+  .get("/", pages.index)
+  .get("/orphanage", pages.orphanage)
+  .get("/orphanages", pages.orphanages)
+  .get("/create-orphanage", pages.createOrphanage);
+
+server.listen(5500);
